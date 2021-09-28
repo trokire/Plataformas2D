5baf4f36b56b6770b5423adf8fea56ef85e43bde
xapamacts <91156866+xapamacts@users.noreply.github.com>
2021-09-28 13:57:08 +0200

    Etapa2 sense UI

--- Assets/Scripts/Characters/PlayerMove.cs
+++ Assets/Scripts/Characters/PlayerMove.cs
@@ -5,24 +5,56 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float runSpeed = 2f;
+    public float jumpForce = 10f;
 
-    private Rigidbody2D body; 
+    private Rigidbody2D body;
+    private Animator anim;
     private float horizontal;
+    private int costat;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        costat = 1;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        horizontal = Input.GetAxisRaw("Horizontal");
-    }
-
     void FixedUpdate()
     {
-        body.velocity = new Vector2(horizontal * runSpeed, 0);
+        if (Input.GetKey(KeyCode.D) || Input.GetKey("right"))
+        {
+            costat = 1;
+            anim.SetBool("walk", true);
+            body.velocity = new Vector2(runSpeed, body.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey("left"))
+        {
+            costat = -1;            
+            anim.SetBool("walk", true);
+
+            body.velocity = new Vector2(-runSpeed, body.velocity.y);
+        }
+        else
+        {
+            body.velocity = new Vector2(0, body.velocity.y);
+            anim.SetBool("walk", false);            
+        }
+
+        if (Input.GetKey(KeyCode.Space) && CheckGround.isGrounded)
+        {
+            body.velocity = new Vector2(body.velocity.x,jumpForce);
+            anim.SetBool("jump", true);
+        }
+        transform.localScale = new Vector3(costat, 1, 1);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Grounds")
+        {
+            anim.SetBool("jump", false);
+        }
     }
 }
