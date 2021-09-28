6635d29d1e411bae9f720df531f32e9e329a91f5
xapamacts <91156866+xapamacts@users.noreply.github.com>
2021-09-28 16:03:31 +0200

    Etapa 2

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -10,12 +10,6 @@ public class CameraFollow : MonoBehaviour
 
     private Vector2 velocity;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void FixedUpdate()
     {
