69427cbb51fa9663bbd3ab6c780c9635d1bd0d92
xapamacts <91156866+xapamacts@users.noreply.github.com>
2021-10-02 19:53:19 +0200

    Etapa3

--- Assets/Scripts/Characters/PlayerControl.cs
+++ Assets/Scripts/Characters/PlayerControl.cs
@@ -7,13 +7,36 @@ public class PlayerControl : MonoBehaviour
 
     public GameObject gameManager;
 
+    private bool isTouch;
+
+    private void Start()
+    {
+        isTouch = false;
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Bomb")
+        if (collision.tag == "Bomb" && isTouch == false)
         {
+            isTouch = true;
             gameManager.GetComponent<SoundManager>().PlayBombFX();
             gameManager.GetComponent<GameManager>().LoseLive();
+            StartCoroutine("WaitState");
         }
+        if (collision.tag == "Stars" && isTouch == false)
+        {
+            isTouch = true;
+            gameManager.GetComponent<GameManager>().AddPoint();
+            gameManager.GetComponent<GameManager>().ShowScore();
+            StartCoroutine("WaitState");
+        }
+    }
+
+    private IEnumerator WaitState()
+    {
+        yield return new WaitForSeconds(0.4f);
+        isTouch = false;
     }
 
 }
--- Assets/Scripts/Characters/PlayerMove.cs
+++ Assets/Scripts/Characters/PlayerMove.cs
@@ -6,6 +6,9 @@ public class PlayerMove : MonoBehaviour
 {
     public float runSpeed = 2f;
     public float jumpForce = 10f;
+    public bool isJoystickRight;
+    public bool isJoystickLeft;
+    public bool isJoystickJump;
 
     private Rigidbody2D body;
     private Animator anim;
@@ -18,18 +21,51 @@ public class PlayerMove : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         costat = 1;
+        isJoystickRight = false;
+        isJoystickLeft = false;
+        isJoystickJump = false;
+    }
+
+    public void JoystickJumpPress()
+    {
+        isJoystickJump = true;
+    }
+
+    public void JoysticJumpRelease()
+    {
+        isJoystickJump = false;
+    }
+
+    public void JoystickRightPress()
+    {
+        isJoystickRight = true;
+    }
+
+    public void JoystickLeftPress()
+    {
+        isJoystickLeft = true;
+    }
+
+    public void JoystickRightRelease()
+    {
+        isJoystickRight = false;
+    }
+
+    public void JoystickLeftRelease()
+    {
+        isJoystickLeft = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.D) || Input.GetKey("right"))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey("right") || isJoystickRight == true)
         {
             costat = 1;
             anim.SetBool("walk", true);
             body.velocity = new Vector2(runSpeed, body.velocity.y);
-        }
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey("left"))
+        } 
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey("left") || isJoystickLeft == true)
         {
             costat = -1;            
             anim.SetBool("walk", true);
@@ -42,7 +78,7 @@ public class PlayerMove : MonoBehaviour
             anim.SetBool("walk", false);            
         }
 
-        if (Input.GetKey(KeyCode.Space) && CheckGround.isGrounded)
+        if (Input.GetKey(KeyCode.Space) && CheckGround.isGrounded || isJoystickJump && CheckGround.isGrounded)
         {
             body.velocity = new Vector2(body.velocity.x,jumpForce);
             anim.SetBool("jump", true);
